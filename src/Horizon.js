cf4b13f2ebba7728725120f0b7839eea7b5d2d12
pyrsmk <dev@dreamysource.fr>
2016-03-27 14:30:55 +0200

    Update version

--- src/Horizon.js
+++ src/Horizon.js
@@ -1,4 +1,4 @@
-/*! Horizon 3.0.2 (https://github.com/pyrsmk/Horizon) */
+/*! Horizon 3.0.3 (https://github.com/pyrsmk/Horizon) */
 
 var W = require('../node_modules/pyrsmk-w/W.min.js'),
 	Horizon = {};
